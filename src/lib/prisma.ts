499c77183c468e3e03c11c0329be9c3d2ba33d1d
Taijul Islam <devtaijul@gmail.com>
2026-03-25 13:52:51 +0600

    Refactor code structure for improved readability and maintainability

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,22 +1,24 @@
-// src/lib/prisma.ts
-
-import { withAccelerate } from "@prisma/extension-accelerate";
 import { PrismaClient } from "../generated/prisma";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
-type PrismaWithAccel = ReturnType<typeof makePrisma>;
-
-const globalForPrisma = global as unknown as { prisma?: PrismaWithAccel };
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
+};
 
 function makePrisma() {
-  // If you need to override the URL per env:
-  // const url = process.env.DATABASE_URL!;
-  const client = new PrismaClient({
-    log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
-    // datasources: { db: { url } }, // optional
-  }).$extends(withAccelerate());
+  const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
 
-  return client;
+  return new PrismaClient({
+    adapter: new PrismaPg(pool),
+    log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
+  });
 }
 
 export const prisma = globalForPrisma.prisma ?? makePrisma();
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
